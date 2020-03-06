72284a9c95c62bb216c312a036ddd419181e1b62
Wojciech Langiewicz <wojciech@wlangiewicz.com>
2020-03-06 09:41:18 +0100

    use -dustrelayfee=0

--- init-node.sh
+++ init-node.sh
@@ -7,12 +7,12 @@ export NODE_NAME="node"
 
 mkdir $NODE_NAME
 
-bitcoind -dns=0 -datadir=$NODE_NAME -rpcport=$RPC_PORT -port=$PORT -regtest=1 -printtoconsole -rpcbind=* -rpcallowip=0.0.0.0/0 -rpcpassword=password -rpcuser=user -txindex=1 -minrelaytxfee=0 &
+bitcoind -dns=0 -datadir=$NODE_NAME -rpcport=$RPC_PORT -port=$PORT -regtest=1 -printtoconsole -rpcbind=* -rpcallowip=0.0.0.0/0 -rpcpassword=password -rpcuser=user -txindex=1 -minrelaytxfee=0 -dustrelayfee=0 &
 
 sleep 5
 
 ADDRESS=$(bitcoin-cli -rpcport=$RPC_PORT -regtest=1 -rpcpassword=password -rpcuser=user getnewaddress)
 
-bitcoin-cli -rpcport=$RPC_PORT -regtest=1 -rpcpassword=password -rpcuser=user generatetoaddress 432 "$ADDRESS"
+bitcoin-cli -rpcport=$RPC_PORT -regtest=1 -rpcpassword=password -rpcuser=user generatetoaddress 101 "$ADDRESS"
 
 fg
