74e1b6b544982e7cc17aa9c62b691a13be78764c
Paweł Jendrusik <p.jendrusik@gmail.com>
2021-02-17 20:27:54 +0100

    wait for bitcoind to be ready to serve new wallet address

--- init-node.sh
+++ init-node.sh
@@ -9,7 +9,11 @@ mkdir $NODE_NAME
 
 bitcoind -dns=0 -datadir=$NODE_NAME -rpcport=$RPC_PORT -port=$PORT -regtest=1 -printtoconsole -rpcbind=* -rpcallowip=0.0.0.0/0 -rpcpassword=password -rpcuser=user -txindex=1 -minrelaytxfee=0 -dustrelayfee=0 &
 
-sleep 5
+while ! bitcoin-cli -rpcport=$RPC_PORT -regtest=1 -rpcpassword=password -rpcuser=user getblockhash 0
+do
+    echo "bitcoind still not ready..."
+    sleep 1
+done
 
 ADDRESS=$(bitcoin-cli -rpcport=$RPC_PORT -regtest=1 -rpcpassword=password -rpcuser=user getnewaddress)
 
